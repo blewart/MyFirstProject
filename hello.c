4c3bd7f36956103f73cbbb9d9adf7475850293f5
blewart <2534234554@qq.com>
2020-10-24 11:22:10 +0800

    a little
    little

--- hello.c
+++ hello.c
@@ -3,9 +3,13 @@ using namespace std;
 int main()
 {
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 	cout<<"i'm ok"<<endl;
 >>>>>>> test
 	cout << "hello" <<"yes ok "<< endl
+=======
+	cout << "hello" << endl
+>>>>>>> master
 		return 0;
 }
